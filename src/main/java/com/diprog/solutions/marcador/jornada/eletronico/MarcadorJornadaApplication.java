acb4dc7afca748f5ceeec8792a72fd8e8992b612
Edfram-Guerra <edfram.guerra@gmail.com>
2024-10-25 23:35:36 -0300

    implementada as conexões com o banco de dados e feito ajustes no programa

---
+++ src/main/java/com/diprog/solutions/marcador/jornada/eletronico/MarcadorJornadaApplication.java
@@ -0,0 +1,13 @@
+package com.diprog.solutions.marcador.jornada.eletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcadorJornadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcadorJornadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diprog/solutions/marcador/jornada/eletronico/MarcadorJornadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diprog.solutions.marcador.jornada.eletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcadorJornadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
