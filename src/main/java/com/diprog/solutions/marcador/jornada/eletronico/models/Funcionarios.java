f81de5d88b8a6f35650cabe1c8ed12366cbc6195
Edfram-Guerra <edfram.guerra@gmail.com>
2024-10-30 20:59:01 -0300

    Implementado model/repository de funcionario

---
+++ src/main/java/com/diprog/solutions/marcador/jornada/eletronico/models/Funcionarios.java
@@ -0,0 +1,80 @@
+package com.diprog.solutions.marcador.jornada.eletronico.models;
+
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table
+public class Funcionarios implements Serializable {
+
+    private static final Long serialVersionUID=1l;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String position;
+    private String department;
+    private Integer cpf;
+    private String senha;
+
+    public Funcionarios() {
+    }
+
+    public Funcionarios(String name, String position, String department, Integer cpf, String senha) {
+        this.name = name;
+        this.position = position;
+        this.department = department;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void registrarPonto(){
+        //construir o metodo apos a criação da classe
+    }
+}
